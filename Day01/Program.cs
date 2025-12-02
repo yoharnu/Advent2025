be021e81b1e0c5e865119527b47a493d384ebca3
yoharnu <dani91@gmail.com>
2025-12-01 20:42:11 -0500

    Day01 part 2

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -2,24 +2,22 @@
 {
     internal class Program
     {
+        public static int partOne = 0;
+        public static int partTwo = 0;
+
         static void Main(string[] args)
         {
             var input = System.IO.File.ReadAllLines("input.txt");
-            PartOne(input);
-        }
-
-        public static void PartOne(string[] input)
-        {
             var current = 50;
-            var zeroCount = 0;
             foreach (var line in input)
             {
                 var direction = line[0];
                 var amount = int.Parse(line[1..]);
                 current = Rotate(current, direction, amount);
-                if (current == 0) zeroCount++;
+                if (current == 0) partOne++;
             }
-            Console.WriteLine($"Final value: {zeroCount}");
+            Console.WriteLine($"Part One value: {partOne}");
+            Console.WriteLine($"Part Two value: {partTwo}");
         }
 
         public static int Rotate(int current, char direction, int amount)
@@ -29,13 +27,19 @@
             {
                 newValue = current + amount;
                 while (newValue >= 100)
+                {
                     newValue -= 100;
+                    partTwo++;
+                }
             }
             else
             {
                 newValue = current - amount;
                 while (newValue < 0)
+                {
                     newValue += 100;
+                    partTwo++;
+                }
             }
             return newValue;
         }
