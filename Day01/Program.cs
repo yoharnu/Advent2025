fc502ef7f177bb7e603e0ace36b1d9d445bf51a5
yoharnu <dani91@gmail.com>
2025-12-01 21:49:11 -0500

    refactor day01 for simplicity

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,47 +1,38 @@
-﻿namespace Day01
+﻿int partOne = 0;
+int partTwo = 0;
+
+var input = System.IO.File.ReadAllLines("input.txt");
+var current = 50;
+foreach (var line in input)
 {
-    internal class Program
+    var direction = line[0];
+    var amount = int.Parse(line[1..]);
+    current = Rotate(current, direction, amount, ref partTwo);
+    if (current == 0) partOne++;
+}
+Console.WriteLine($"Part One value: {partOne}");
+Console.WriteLine($"Part Two value: {partTwo}");
+
+static int Rotate(int current, char direction, int amount, ref int partTwo)
+{
+    var newValue = current;
+    if (direction == 'R')
     {
-        public static int partOne = 0;
-        public static int partTwo = 0;
-
-        static void Main(string[] args)
+        newValue = current + amount;
+        while (newValue >= 100)
         {
-            var input = System.IO.File.ReadAllLines("input.txt");
-            var current = 50;
-            foreach (var line in input)
-            {
-                var direction = line[0];
-                var amount = int.Parse(line[1..]);
-                current = Rotate(current, direction, amount);
-                if (current == 0) partOne++;
-            }
-            Console.WriteLine($"Part One value: {partOne}");
-            Console.WriteLine($"Part Two value: {partTwo}");
-        }
-
-        public static int Rotate(int current, char direction, int amount)
-        {
-            var newValue = current;
-            if (direction == 'R')
-            {
-                newValue = current + amount;
-                while (newValue >= 100)
-                {
-                    newValue -= 100;
-                    partTwo++;
-                }
-            }
-            else
-            {
-                newValue = current - amount;
-                while (newValue < 0)
-                {
-                    newValue += 100;
-                    partTwo++;
-                }
-            }
-            return newValue;
+            newValue -= 100;
+            partTwo++;
         }
     }
+    else
+    {
+        newValue = current - amount;
+        while (newValue < 0)
+        {
+            newValue += 100;
+            partTwo++;
+        }
+    }
+    return newValue;
 }
