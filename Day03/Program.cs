959584fbf7a5f4a8a76b46988e495c76d8b4991e
yoharnu <dani91@gmail.com>
2025-12-03 08:19:35 -0500

    Day03 Part 1

---
+++ Day03/Program.cs
@@ -0,0 +1,23 @@
+﻿var input = System.IO.File.ReadAllLines("input.txt");
+long runningTotal = 0;
+foreach (var line in input)
+{
+    runningTotal += CalculateJoltage(line);
+}
+
+Console.WriteLine($"Total Joltage: {runningTotal}");
+
+long CalculateJoltage(string line)
+{
+    var bank = ConvertLineToBank(line);
+    var digitOne = bank.Take(bank.Count - 1).Max();
+    var digitOneIndex = bank.IndexOf(digitOne);
+    var digitTwo = bank[(digitOneIndex + 1)..].Max();
+    var joltage = digitOne * 10 + digitTwo;
+    return joltage;
+}
+
+List<long> ConvertLineToBank(string line)
+{
+    return line.Select(x => long.Parse(x.ToString())).ToList();
+}
